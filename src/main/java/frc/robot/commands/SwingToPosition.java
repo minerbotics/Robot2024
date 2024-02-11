92c2bf1257fe9b9da355151e716cac2bf8ac05d6
Michael Kinnunen <michaelkinnunen@gmail.com>
2024-02-11 13:04:42 -0500

    Simplify SwingToPosition a bit

--- src/main/java/frc/robot/commands/SwingToPosition.java
+++ src/main/java/frc/robot/commands/SwingToPosition.java
@@ -8,6 +8,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 public class SwingToPosition extends Command {
   private final Swinger m_Swinger;
   private final int m_goalType;
+  private double targetPosition;
 
   public SwingToPosition(Swinger swinger, int goalType) {
     m_Swinger = swinger;
@@ -19,64 +20,31 @@ public class SwingToPosition extends Command {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    double position = 0;
     switch (m_goalType) {
       case GoalTypeConstants.AMP:
-        position = SwingConstants.AMP_POSITION;
+        this.targetPosition = SwingConstants.AMP_POSITION;
         break;
       case GoalTypeConstants.SPEAKER:
-        position = SwingConstants.SPEAKER_POSITION;
+        this.targetPosition = SwingConstants.SPEAKER_POSITION;
         break;
       case GoalTypeConstants.SOURCE_1:
       case GoalTypeConstants.SOURCE_2:
       case GoalTypeConstants.SOURCE_3:
-        position = SwingConstants.SOURCE_POSITION;
+        this.targetPosition = SwingConstants.SOURCE_POSITION;
         break;
       case GoalTypeConstants.TRAP:
-        position = SwingConstants.TRAP_POSITION;
+        this.targetPosition = SwingConstants.TRAP_POSITION;
         break;
       default:
-        position = 0;
+        this.targetPosition = 0;
         break;
     }
-    m_Swinger.swingToPosition(position);
+    m_Swinger.swingToPosition(this.targetPosition);
   }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return inPosition(m_goalType);
-  }
-
-  public boolean inPosition(int goalType) {
-    boolean inPosition = false;
-    switch (goalType) {
-      case GoalTypeConstants.AMP:
-        if (m_Swinger.getPosition() == SwingConstants.AMP_POSITION) {
-          inPosition = true;
-        }
-        break;
-      case GoalTypeConstants.SOURCE_1:
-      case GoalTypeConstants.SOURCE_2:
-      case GoalTypeConstants.SOURCE_3:
-        if (m_Swinger.getPosition() == SwingConstants.SOURCE_POSITION) {
-          inPosition = true;
-        }
-        break;
-      case GoalTypeConstants.SPEAKER:
-        if (m_Swinger.getPosition() == SwingConstants.SPEAKER_POSITION) {
-          inPosition = true;
-        }
-        break;
-      case GoalTypeConstants.TRAP:
-        if (m_Swinger.getPosition() == SwingConstants.TRAP_POSITION) {
-          inPosition = true;
-        }
-        break;
-      default:
-        inPosition = false;
-        break;
-    }
-    return inPosition;
+    return (Math.abs(m_Swinger.getPosition() - this.targetPosition) <= SwingConstants.SWING_POSITION_TOLERANCE);
   }
 }
