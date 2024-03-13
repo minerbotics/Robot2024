e851912def4a9686847848e0e11c2861c8257d61
koolguy906 <koolguy906@gmail.com>
2024-03-13 19:13:15 -0400

    New AutoAmpScore Command

---
+++ src/main/java/frc/robot/commands/AutoAmpScore.java
@@ -0,0 +1,27 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.Constants.GoalTypeConstants;
+import frc.robot.subsystems.IntakeSubsystem;
+import frc.robot.subsystems.Shooter;
+import frc.robot.subsystems.Swerve;
+import frc.robot.subsystems.Swinger;
+
+public class AutoAmpScore extends SequentialCommandGroup {
+    private Swerve m_Swerve;
+    private Swinger m_Swinger;
+    private IntakeSubsystem m_Intake;
+    private Shooter m_Shooter;
+    
+    public AutoAmpScore(Swerve swerve, Swinger swinger, IntakeSubsystem intake, Shooter shooter){
+        m_Swerve = swerve;
+        m_Swinger = swinger;
+        m_Intake = intake;
+        m_Shooter = shooter;
+        addCommands(
+            new ManeuverOn(m_Swerve, GoalTypeConstants.AMP).withTimeout(3),
+            new SwingToPosition(m_Swinger, GoalTypeConstants.AMP),
+            new DoShoot(m_Intake, m_Shooter, GoalTypeConstants.AMP)
+        );
+    }
+}
