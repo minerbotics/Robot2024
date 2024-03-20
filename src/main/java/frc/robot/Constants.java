06f96546b4433107498ba5970553b2172ee6e742
Michael Kinnunen <michaelkinnunen@gmail.com>
2024-03-20 18:49:13 -0400

    Update motor variables

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -63,8 +63,8 @@ public final class Constants {
   }
 
   public static class ShooterConstants {
-    public static final int TOP_SHOOTER_MOTOR = 20;
-    public static final int MID_SHOOTER_MOTOR = 21;
+    public static final int MID_SHOOTER_MOTOR = 20;
+    public static final int BOTTOM_SHOOTER_MOTOR = 21;
     public static final int TOP_RIGHT_SHOOTER_MOTOR = 22;
     public static final int TOP_LEFT_SHOOTER_MOTOR = 26;
   }
--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -7,43 +7,41 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.ShooterConstants;
 
 public class Shooter extends SubsystemBase {
-  private final CANSparkMax m_TopShooterMotor, m_MidShooterMotor, m_TopRightShooterMotor, m_TopLeftShooterMotor;
+  private final CANSparkMax m_MidShooterMotor, m_BottomShooterMotor, m_TopRightShooterMotor, m_TopLeftShooterMotor;
 
   public Shooter() {
-    m_TopShooterMotor = new CANSparkMax(ShooterConstants.TOP_SHOOTER_MOTOR, CANSparkLowLevel.MotorType.kBrushless);
     m_MidShooterMotor = new CANSparkMax(ShooterConstants.MID_SHOOTER_MOTOR, CANSparkLowLevel.MotorType.kBrushless);
+    m_BottomShooterMotor = new CANSparkMax(ShooterConstants.BOTTOM_SHOOTER_MOTOR, CANSparkLowLevel.MotorType.kBrushless);
     m_TopRightShooterMotor = new CANSparkMax(ShooterConstants.TOP_RIGHT_SHOOTER_MOTOR, CANSparkLowLevel.MotorType.kBrushless);
     m_TopLeftShooterMotor = new CANSparkMax(ShooterConstants.TOP_LEFT_SHOOTER_MOTOR, CANSparkLowLevel.MotorType.kBrushless);
     m_TopRightShooterMotor.follow(m_TopLeftShooterMotor, true);
   }
 
   public void ampOut() {
-    m_TopRightShooterMotor.set(0.25);
     m_MidShooterMotor.set(0.25);
+    m_BottomShooterMotor.set(0.25);
   }
 
   public void speakerOut() {
-    m_TopShooterMotor.set(-1);
     m_TopLeftShooterMotor.set(-1);
-    m_MidShooterMotor.set(-0.25);
-    m_TopRightShooterMotor.set(-0.25);
+    m_MidShooterMotor.set(-0.5);
+    m_BottomShooterMotor.set(-0.25);
   }
 
   public void intakeIn() {
-    m_TopRightShooterMotor.set(-0.5);
+    m_BottomShooterMotor.set(-0.5);
     m_MidShooterMotor.set(0);
-    m_TopShooterMotor.set(0);
+    m_TopLeftShooterMotor.set(0);
   }
 
   public void spinTop() {
-    m_TopShooterMotor.set(-1);
     m_TopLeftShooterMotor.set(-1);
   }
 
   public void stop() {
     m_TopRightShooterMotor.set(0);
+    m_BottomShooterMotor.set(0);
     m_MidShooterMotor.set(0);
-    m_TopShooterMotor.set(0);
     m_TopLeftShooterMotor.set(0);
   }
 }
