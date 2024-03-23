668f86da6a50007df660362828776f2420bd5181
Michael Kinnunen <michaelkinnunen@gmail.com>
2024-03-23 11:27:55 -0400

    Remove unused imports

--- src/main/java/frc/robot/commands/AutoSpeakerScoreLeft.java
+++ src/main/java/frc/robot/commands/AutoSpeakerScoreLeft.java
@@ -1,7 +1,6 @@
 package frc.robot.commands;
 
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Constants.GoalTypeConstants;
 import frc.robot.subsystems.IntakeSubsystem;
